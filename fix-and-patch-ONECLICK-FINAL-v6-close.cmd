895e0f7b767b8c9046a41925e8ffb56de8ed060c
Novimetrix <mohamed.elmalik@gmail.com>
2025-10-26 12:56:10 +0400

    100

--- fix-and-patch-ONECLICK-FINAL-v6-close.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set "HERE=%~dp0"
-set "PSX=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
-"%PSX%" -ExecutionPolicy Bypass -File "%HERE%fix-and-patch-ONECLICK-FINAL-v6-close.ps1"
