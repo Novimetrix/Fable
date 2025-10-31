8d3884678d05a5aebf73051a5b571d1ad83164f2
Novimetrix <mohamed.elmalik@gmail.com>
2025-10-31 20:50:02 +0400

    100

--- fix-and-patch-ONECLICK-FINAL-v5-PS5-UTF8-FIX.cmd
+++ fix-and-patch-ONECLICK-FINAL-v5-PS5-UTF8-FIX.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set "HERE=%~dp0"
-set "PS1=%HERE%fix-and-patch-ONECLICK-FINAL-v5-PS5-UTF8-FIX+RESTLINK+STICKY.ps1"
+set "PS1=%HERE%fix-and-patch-ONECLICK-FINAL-v5-PS5-UTF8-FIX.ps1"
 set "PS=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
 if not exist "%PS1%" (
   echo PowerShell file not found: "%PS1%"
