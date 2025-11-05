d55c46d2ca932b59f336db8160af6caa2834616c
Novimetrix <mohamed.elmalik@gmail.com>
2025-11-05 21:14:39 +0400

    100

--- RUN-FLASHGUARD.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-REM Run the FlashGuard patch from the folder it's placed in
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0fix-and-patch-ONECLICK-FLASHGUARD-v6.ps1"
-echo.
-echo Press any key to exit...
-pause >nul
-endlocal
