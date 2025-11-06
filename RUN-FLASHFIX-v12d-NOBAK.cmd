fe9d1a20d5c599ac6636ac6d2c5e402106fd87aa
Novimetrix <mohamed.elmalik@gmail.com>
2025-11-06 14:30:22 +0400

    100

---
+++ RUN-FLASHFIX-v12d-NOBAK.cmd
@@ -0,0 +1,2 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0nm-flashfix-v12d-nobak.ps1"
--- RUN-SOFTEN-DESKTOP-NAV-v12c.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0soften-desktop-nav-v12c.ps1"
