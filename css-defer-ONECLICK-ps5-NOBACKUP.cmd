1132a8894616536203b01197f951ef0b5acde6b9
Novimetrix <mohamed.elmalik@gmail.com>
2025-11-08 11:50:43 +0400

    100

--- css-defer-ONECLICK-ps5-NOBACKUP.cmd
+++ css-defer-ONECLICK-ps5-NOBACKUP.cmd
@@ -1,6 +1,2 @@
 @echo off
-REM css-defer-ONECLICK-ps5-NOBACKUP.cmd (FORCE-UNBLOCK)
-setlocal
-set SCRIPT=%~dp0css-defer-ONECLICK-ps5-NOBACKUP.ps1
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT%"
-endlocal
+powershell -ExecutionPolicy Bypass -NoLogo -NoProfile -File "%~dp0css-defer-ONECLICK-ps5-NOBACKUP.ps1"
